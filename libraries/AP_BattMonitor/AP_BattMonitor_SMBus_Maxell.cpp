c896c1bead87f7bc8804786511bdd42aec9ae387
Sho <daxjps@gmail.com>
2017-05-16 17:54:58 +0900

    Revert "AP_BattMonitor: confirm pec support before attempting to read voltage"
    This reverts commit dcc4f69f7c98b98939168310e0ef91880e8deb7a.

--- libraries/AP_BattMonitor/AP_BattMonitor_SMBus_Maxell.cpp
+++ libraries/AP_BattMonitor/AP_BattMonitor_SMBus_Maxell.cpp
@@ -15,7 +15,12 @@ extern const AP_HAL::HAL& hal;
 #define BATTMONITOR_SMBUS_MAXELL_SPECIFICATION_INFO    0x1a    // specification info
 #define BATTMONITOR_SMBUS_MAXELL_MANUFACTURE_NAME  0x20    // manufacturer name
 
-#define SMBUS_READ_BLOCK_MAXIMUM_TRANSFER    0x20   // A Block Read or Write is allowed to transfer a maximum of 32 data bytes.
+#define BATTMONITOR_SMBUS_10_PEC_NOT_SUPPORT  0x10  // Smart Battery Specification v1.0
+#define BATTMONITOR_SMBUS_11_PEC_NOT_SUPPORT  0x21  // Smart Battery Specification v1.1 without PEC support
+#define BATTMONITOR_SMBUS_11_PEC_SUPPORT      0x31  // Smart Battery Specification v1.1 with PEC support
+
+// A Block Read or Write is allowed to transfer a maximum of 32 data bytes.
+#define READ_BLOCK_MAXIMUM_TRANSFER    0x20
 
 #define SMBUS_PEC_POLYNOME  0x07 // Polynome for CRC generation
 
@@ -59,9 +64,9 @@ void AP_BattMonitor_SMBus_Maxell::read()
 
 void AP_BattMonitor_SMBus_Maxell::timer()
 {
-	// check if PEC is supported
-    if (!check_pec_support()) {
-        return;
+	// _pec_confirmed set true after confirming if it support PEC
+    if (!_pec_confirmed) {
+        _pec_confirmed = get_pec_support();
     }
 
     uint16_t data;
@@ -101,7 +106,7 @@ bool AP_BattMonitor_SMBus_Maxell::read_word(uint8_t reg, uint16_t& data) const
     }
 
     // check PEC
-    if (_pec_supported) {
+    if (_pec_support) {
         const uint8_t pec = get_PEC(AP_BATTMONITOR_SMBUS_I2C_ADDR, reg, true, buff, 2);
         if (pec != buff[2]) {
             return false;
@@ -126,7 +131,7 @@ uint8_t AP_BattMonitor_SMBus_Maxell::read_block(uint8_t reg, uint8_t* data, bool
     }
 
     // sanity check length returned by smbus
-    if (bufflen == 0 || bufflen > SMBUS_READ_BLOCK_MAXIMUM_TRANSFER) {
+    if (bufflen == 0 || bufflen > READ_BLOCK_MAXIMUM_TRANSFER) {
         return 0;
     }
 
@@ -140,7 +145,7 @@ uint8_t AP_BattMonitor_SMBus_Maxell::read_block(uint8_t reg, uint8_t* data, bool
     }
 
     // check PEC
-    if (_pec_supported) {
+    if (_pec_support) {
         uint8_t pec = get_PEC(AP_BATTMONITOR_SMBUS_I2C_ADDR, reg, true, buff, bufflen+1);
         if (pec != buff[bufflen+1]) {
             return 0;
@@ -159,46 +164,44 @@ uint8_t AP_BattMonitor_SMBus_Maxell::read_block(uint8_t reg, uint8_t* data, bool
     return bufflen;
 }
 
-// check if PEC supported with the version value in SpecificationInfo() function
-// returns true once PEC is confirmed as working or not working
-bool AP_BattMonitor_SMBus_Maxell::check_pec_support()
+// get PEC support using the version value in SpecificationInfo
+bool AP_BattMonitor_SMBus_Maxell::get_pec_support()
 {
-    // exit immediately if we have already confirmed pec support
-    if (_pec_confirmed) {
-        return true;
-    }
+    uint16_t data;
+    uint8_t buff[READ_BLOCK_MAXIMUM_TRANSFER + 1];
 
     // specification info
-    uint16_t data;
     if (!read_word(BATTMONITOR_SMBUS_MAXELL_SPECIFICATION_INFO, data)) {
+        _pec_support = false;
         return false;
     }
 
-    // extract version
-    uint8_t version = (data & 0xF0) >> 4;
+    // determine _pec_support is false when SpecInfo indicates no PEC support
+    if (((data & 0xFF) == BATTMONITOR_SMBUS_10_PEC_NOT_SUPPORT) || ((data & 0xFF) == BATTMONITOR_SMBUS_11_PEC_NOT_SUPPORT)) {
+        _pec_support = false;
+        return true;
+    } else if ((data & 0xFF) != BATTMONITOR_SMBUS_11_PEC_SUPPORT) {
+        _pec_support = false;
+        return false;
+    }
 
-    // version less than 0011b (i.e. 3) do not support PEC
-    if (version < 3) {
-        _pec_supported = false;
-        _pec_confirmed = true;
+    // At first, set true. In the second time, determine it.
+    // This confirm to get the correct value with PEC support
+    if (_pec_support) {
         return true;
     }
 
-    // check manufacturer name
-    uint8_t buff[SMBUS_READ_BLOCK_MAXIMUM_TRANSFER + 1];
+    // manufacturer name
     if (read_block(BATTMONITOR_SMBUS_MAXELL_MANUFACTURE_NAME, buff, true)) {
-        // Hitachi maxell batteries do not support PEC
+        // In Hitachi maxell battery, specification info is 0x31 (SBSv1.1 with PEC support) but PEC isn't support
         if (strcmp((char*)buff, "Hitachi maxell") == 0) {
-            _pec_supported = false;
-            _pec_confirmed = true;
+            _pec_support = false;
             return true;
         }
     }
 
-    // assume all other batteries support PEC
-	_pec_supported = true;
-	_pec_confirmed = true;
-	return true;
+	_pec_support = true;
+    return false;
 }
 
 /// get_PEC - calculate packet error correction code of buffer
